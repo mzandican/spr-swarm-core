ee8804c91c9c8c0ae50c98cd85a3342ad9bba155
mzandican <mnguni.zandile@gmail.com>
2026-04-28 22:14:22 +0200

    Create index.ts file in src directory

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Agent } from './swarm/agent';
+export { WorkerAgent } from './swarm/worker';
+export { ScoutAgent } from './swarm/scout';
+export { Pheromone } from './core/pheromone';
+export { reproductionLoop } from './swarm/reproduce';
